5b15bdf91bb158dfbbde4929d745bca117d5d21b
WJL-git <1229426058@qq.com>
2018-07-22 20:28:37 +0800

    test

---
+++ src/HiWord.java
@@ -0,0 +1,11 @@
+/**
+ * zz
+ *
+ * @author wangjianlong Email:wangjianlong@co-mall.com
+ * @since 2018/7/22 20:26
+ */
+public class HiWord {
+    public static void main(String[] args) {
+        System.out.println("zz");
+    }
+}
